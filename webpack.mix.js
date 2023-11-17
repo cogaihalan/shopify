3e1958719f93a0607e41d5caf1ff129ba05b6e70
Hoang Tai <taihv@tigren.com>
2023-11-17 17:03:31 +0700

    update tailwind & alpinejs

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+let mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+
+mix.js('src/app.js', 'assets')
+    .css('src/app.css', 'assets')
+    .options({
+        processCssUrls: false,
+        postCss: [ tailwindcss('tailwind.config.js') ]
+    })
